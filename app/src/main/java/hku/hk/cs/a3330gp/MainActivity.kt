af41b7c76c0f37b0343aa89488e05b81f99d19a4
tom <b172583552@gmail.com>
2023-11-11 02:29:45 +0800

    add 3 buttons

--- app/src/main/java/hku/hk/cs/a3330gp/MainActivity.kt
+++ app/src/main/java/hku/hk/cs/a3330gp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +13,13 @@ class MainActivity : AppCompatActivity() {
         val health =  findViewById<Button>(R.id.health)
         val profile =  findViewById<Button>(R.id.profile)
         val ar = findViewById<Button>(R.id.ar)
+        val topAppBar = findViewById<MaterialToolbar>(R.id.topAppBar)
+
+        topAppBar.setNavigationOnClickListener {
+            // Handle navigation icon press
+            onBackPressedDispatcher.onBackPressed()
+
+        }
 
         health.setOnClickListener{
             startActivity(Intent(this, HealthStatistics::class.java))
